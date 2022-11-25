e0e123f7c0f474b8435308a54f9bd67c8feb47dd
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-25 18:35:41 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainer = document.querySelector('#ingredients');
+
+const ingredientsList = ingredients.map((item) => {
+  const itemEl = document.createElement('li');
+
+  itemEl.classList.add('item');
+  itemEl.textContent = item;
+
+  return itemEl;
+});
+
+ingredientsContainer.append(...ingredientsList);
