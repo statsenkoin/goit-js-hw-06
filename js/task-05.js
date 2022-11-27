4d526dc24f4053a3922e7ccfeaf78dd816e1e193
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-27 22:46:50 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const nameInput = document.querySelector('#name-input');
+console.log(nameInput.textContent);
+
+const nameOutput = document.querySelector('#name-output');
+console.log(nameOutput.textContent);
+
+const defaultOutput = nameOutput.textContent;
+console.log(defaultOutput);
