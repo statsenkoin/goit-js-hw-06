465f31098e39f087187ea491b16bf3aa9bc8b5e5
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-28 22:37:00 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonChangeColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+const bodyElement = document.querySelector('body');
+
+buttonChangeColor.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const newColor = getRandomHexColor();
+  textColor.textContent = newColor;
+  bodyElement.style.backgroundColor = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
