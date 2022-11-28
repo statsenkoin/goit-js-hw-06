5041c6381261704c21c8fbf8a1b7c0a724f9038e
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-28 22:36:57 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert('Fill all required fields');
+  } else {
+    console.log({ email, password });
+    form.reset();
+  }
+}
