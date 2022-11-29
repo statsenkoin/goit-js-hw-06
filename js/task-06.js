6d4912763cc1c0f2f4a7a7895febd55c967a8a86
Ihor Statsenko <statsenkoin@gmail.com>
2022-11-29 19:14:36 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,18 @@
 const inputEl = document.querySelector('#validation-input');
 
-// inputEl.addEventListener('focus', (e) => {
-//   e.currentTarget.value = '';
-//   inputEl.classList.remove('valid', 'invalid');
-// });
-
 inputEl.addEventListener('blur', (e) => {
-  if (e.currentTarget.value.length === Number(inputEl.dataset.length)) {
-    inputEl.classList.add('valid');
-    inputEl.classList.remove('invalid');
-  } else {
-    inputEl.classList.add('invalid');
-    inputEl.classList.remove('valid');
-  }
+  inputEl.classList.add('invalid');
+  e.currentTarget.value.length === Number(inputEl.dataset.length)
+    ? inputEl.classList.replace('invalid', 'valid')
+    : inputEl.classList.replace('valid', 'invalid');
 });
+
+// inputEl.addEventListener('blur', (e) => {
+//   if (e.currentTarget.value.length === Number(inputEl.dataset.length)) {
+//     inputEl.classList.add('valid');
+//     inputEl.classList.remove('invalid');
+//   } else {
+//     inputEl.classList.add('invalid');
+//     inputEl.classList.remove('valid');
+//   }
+// });
