af5a2df0c3d05fc4c7d7065efc51ce8358887f8b
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-01 19:33:44 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputFontSizeControl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
-textEl.style.fontSize = inputFontSizeControl.value + 'px';
+textEl.style.fontSize = inputFontSizeControl.value + 'px'; //bugFix
 
 inputFontSizeControl.addEventListener('input', (e) => {
   textEl.style.fontSize = e.currentTarget.value + 'px';
